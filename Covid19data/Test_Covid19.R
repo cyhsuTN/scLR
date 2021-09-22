67e6dedfbf069528547aa4325e01a0d6c3a1cee4
Chih-Yuan Hsu <53067662+cyhsuTN@users.noreply.github.com>
2021-09-22 17:46:34 -0500

    R code

---
+++ Covid19data/Test_Covid19.R
@@ -0,0 +1,78 @@
+#if (!requireNamespace("BiocManager", quietly = TRUE))
+#  install.packages("BiocManager")
+
+#BiocManager::install("limma")
+#BiocManager::install("DESeq2")
+
+library(DESeq2)
+library(scLR)
+library(dplyr)
+options(scipen = 999)
+setwd("C:/Users/farca/Desktop/Project_SingleCellSpatial/Data")
+
+## LR pairs list
+lrpairs <- read.table('singlecellsignalR_LRdb.txt', header = TRUE)
+lrpairs <- distinct(lrpairs, ligand, receptor, .keep_all= TRUE)
+
+load('COVID19.RData')
+
+#head(counts); dim(counts)
+#head(SampleInfo); dim(SampleInfo)
+cluster.name <- unique(SampleInfo$cluster)
+gene.name <- toupper(rownames(counts))
+
+output <- scLR(countmatrix=counts,
+               cellinfo=SampleInfo,
+               lrpairs.sample=lrpairs,
+               low.filter = 1, p.adjust.method = "BH",
+               Brep = 1000, Brep0 = 200, pv0 = 0.05, p.filter = 0.10, show.all = FALSE,
+               parallel.use = TRUE, cpucores = 4, zero.impute = TRUE, adjust = 3, rhos = 0,
+               normalization = "ByALL", impute.miss.celltype = NA)
+RR <- output$Rs
+
+
+### Only show cases with adj.p < 0.01
+FRR <- RR[(RR$adj.p < 0.01),1:11]
+#write.csv(FRR, file = "COVID19_ByALL0.csv", row.names = F)
+
+
+### Show significant lr pairs with means of ligands and receptors > 1
+idx.ligand <- cbind(pmatch(sub("(.*)-.*", "\\1",FRR[,2]), gene.name, duplicates.ok = T),
+                    pmatch(sub("(.*)-.*", "\\1",FRR[,1]), cluster.name, duplicates.ok = T))
+idx.receptor <- cbind(pmatch(sub(".*-", "\\",FRR[,2]), gene.name, duplicates.ok = T),
+                    pmatch(sub(".*-", "\\",FRR[,1]), cluster.name, duplicates.ok = T))
+aftermean1 <- (output$mu.matrix1[idx.ligand] * 2 + 5 * output$mu.matrix2[idx.ligand])/7
+aftermean2 <- (output$mu.matrix1[idx.receptor] * 2 + 5 * output$mu.matrix2[idx.receptor])/7
+FRR2 <- FRR[which(aftermean1 > 1 & aftermean2 > 1),1:11]
+#write.csv(FRR2, file = "COVID19_ByALL.csv", row.names = F)
+
+
+### Further exclude Unknown cell type
+idx.unknown <- which(sub("(.*)-.*", "\\1",FRR2[,1])=="Unknown" | sub(".*-", "\\",FRR2[,1])=="Unknown")
+FRR3 <- FRR2[-idx.unknown,1:11]
+write.csv(FRR3, file = "COVID19_ByALL_rmUnknown.csv", row.names = F)
+
+
+
+### Figure 2
+library(tidyr)
+library(pheatmap)
+lr_short <- spread(FRR3[,1:3], lr.gene.name, obs.xy.diff, fill=0)
+rownames(lr_short) <- lr_short$lr.cell.name
+lr_short <- lr_short[,-1]
+
+#remove epithelial cells
+epithelialind <- grep("Ciliated|Alveolar", rownames(lr_short))
+lr_immune <- lr_short[-epithelialind,]
+removeLR <- apply(lr_immune,2,sum)==0
+lr_immune <- lr_immune[,!removeLR]
+
+#CCL
+pheatmap(lr_immune[,grep("CCL",colnames(lr_immune))],fontsize_col = 6,fontsize_row=6,cluster_cols = F)
+
+#CXCL
+pheatmap(lr_immune[,grep("CXCL",colnames(lr_immune))],fontsize_col = 6,fontsize_row=6,cluster_cols = F)
+
+#IFNG
+pheatmap(lr_immune[,grep("IFNG",colnames(lr_immune))],fontsize_col = 6,fontsize_row=6,cluster_cols = F)
+
