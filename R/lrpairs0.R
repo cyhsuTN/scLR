ecc13ed49cd687716016b083e82bc1e38533fca6
Chih-Yuan Hsu <53067662+cyhsuTN@users.noreply.github.com>
2026-06-05 13:41:44 -0500

    Create lrpairs0.R

---
+++ R/lrpairs0.R
@@ -0,0 +1,10 @@
+#' A dataset storing ligand-receptor pairs for comparisons
+#'
+#' @description A dataset storing ligand-receptor pairs for comparisons
+#'
+#' @format A data frame with 3239 rows (LR pairs) and 2 columns:
+#' \describe{
+#'   \item{ligand}{ligand name}
+#'   \item{receptor}{receptor name}
+#' }
+"lrpairs0"
